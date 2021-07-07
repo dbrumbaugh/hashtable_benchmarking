23b2b16503b67e43627350e1324c4ebd94aa06ad
Douglas Rumbaugh <doug@douglasrumbaugh.com>
2021-07-07 11:24:48 -0400

    Fixed errors in runner
    Missed a $ in the for loop, and echo needs -e to process backslash
    escapes.

--- benchmarks/bench_run.sh
+++ benchmarks/bench_run.sh
@@ -2,11 +2,11 @@
 echo "Running benchmarks"
 
 
-echo "\tRunning baseline benchmark"
+echo -e "\tRunning baseline benchmark"
 echo "n,throughput,latency" > baseline.log
-for (( i = 1; i<1000000; i = i + 10000 )); do
+for (( i = 1; i<1000000; i = $i + 10000 )); do
     ./baseline_bench $i >> baseline.log
 done
-echo "\tFinished running baseline benchmark"
+echo -e "\tFinished running baseline benchmark"
 
 echo ""
