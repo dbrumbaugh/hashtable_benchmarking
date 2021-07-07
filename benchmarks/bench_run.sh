eef3dad0c8de6f788366ca619ed78246156b87b0
Douglas Rumbaugh <doug@douglasrumbaugh.com>
2021-07-07 13:43:01 -0400

    More script fixes

--- benchmarks/bench_run.sh
+++ benchmarks/bench_run.sh
@@ -3,25 +3,25 @@ echo "Running benchmarks"
 
 
 echo -e "\tRunning baseline benchmark"
-echo "n,throughput,latency" > baseline.log
+echo "n,throughput,latency" > ./benchmarks/baseline.log
 for (( i = 1; i<1000000; i = $i + 10000 )); do
-    ./benchmarks/baseline_bench $i >> baseline.log
+    ./benchmarks/baseline_bench $i >> ./benchmarks/baseline.log
 done
 echo -e "\tFinished running baseline benchmark"
 
 echo -e "\tRunning stride benchmark"
-echo "stride,throughput,latency" > stride.log
+echo "stride,throughput,latency" > ./benchmarks/stride.log
 for (( i = 1; i<100; i = $i + 1 )); do
-    ./benchmarks/stride_bench $i 100000 >> stride.log
+    ./benchmarks/stride_bench $i 100000 >> ./benchmarks/stride.log
 done
 echo -e "\tFinished running stride benchmark"
 
 
 echo -e "\tRunning lf benchmark"
-echo "lf,throughput,latency" > load.log
+echo "lf,throughput,latency" > ./benchmarks/load.log
 for (( i = 50; i<91; i = $i + 1 )); do
     lf=$(( $i / 100.0 ))
-    ./benchmarks/load_bench $lf 10000 >> load.log
+    ./benchmarks/load_bench $lf 10000 >> ./benchmarks/load.log
 done
 echo -e "\tFinished running lf benchmark"
 
