81bd2fd0dde1424aa1a9e609d06e1bc73d4a7f7d
Douglas Rumbaugh <doug@douglasrumbaugh.com>
2021-07-07 13:40:12 -0400

    More script fixes

--- benchmarks/bench_run.sh
+++ benchmarks/bench_run.sh
@@ -5,7 +5,7 @@ echo "Running benchmarks"
 echo -e "\tRunning baseline benchmark"
 echo "n,throughput,latency" > baseline.log
 for (( i = 1; i<1000000; i = $i + 10000 )); do
-    ./benchmarks/baseline_bench $i >> baseline.log
+    ./benchmarks/baseline_bench $i >> ./benchmarks/baseline.log
 done
 echo -e "\tFinished running baseline benchmark"
 
