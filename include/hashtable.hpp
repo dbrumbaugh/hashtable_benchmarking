aefb226bf88c75c54767cb627808ac6fcb5b4dc9
Douglas Rumbaugh <doug@douglasrumbaugh.com>
2021-07-13 15:51:24 -0400

    Committing to move to other computer

--- include/hashtable.hpp
+++ include/hashtable.hpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <cstdlib>
 #include <cstdio>
 
@@ -22,99 +23,107 @@ struct alignas(64) element_t {
     EStatus status;
 };
 
+typedef bool hashmode_t;
+#define hash0 false
+#define hash1 true
+
 
 template <typename TKey, typename TValue>
 class HashTable {
     private:
-        int count;
-        int size;
-        int stride;
-        element_t<TKey, TValue> *elements;
+        size_t count;
+        size_t size;
+        size_t partial_sz;
+        element_t<TKey, TValue> *elements_0;
+        element_t<TKey, TValue> *elements_1;
 
         u_int32_t (*h0)(TKey);
-        double max_lf;
+        u_int32_t (*h1)(TKey);
+        size_t max_itr;
 
 
         element_t<TKey, TValue> *operate(TKey key, Operation op)
         {
-            auto idx = hash(key);
+            auto idx0 = hash(key, hash0);
+            auto idx1 = hash(key, hash1);
+            element_t<TKey, TValue> *temp0 = &(elements_0[idx0]);
+            element_t<TKey, TValue> *temp1 = &(elements_1[idx1]);
+            element_t<TKey, TValue> *element = nullptr;
 
-            element_t<TKey, TValue> *temp = &elements[idx];
+            if (temp0->key == key && temp0->status == ES_POPULATED) element = temp0;
+            if (temp1->key == key && temp1->status == ES_POPULATED) element = temp1;
 
-            // TODO: This will classify a deleted element as "found" still,
-            // but I want to get other stuff working first, before I deal with
-            // that edge case
-            while (temp->key != key && temp->status != ES_EMPTY) {
-                idx = (idx + stride) % size;
-                temp = &(elements[idx]);
+            if (op == O_ACCESS) return element;
+
+            if (op == O_DELETE && element) {
+                element->status = ES_DELETED;
+                return nullptr;
             }
 
-            if (temp->status == ES_EMPTY || temp->status == ES_DELETED)
-                temp = nullptr;
-
-            if (op == O_ACCESS)
-                return temp;
-
-            // We need the tombstones to still count for load_factor calculation,
-            // so don't decrement the count variable here.
-            if (op == O_DELETE && temp) {
-                temp->status = ES_DELETED;
-            }
-
-            return nullptr;
+            return element;
         }
 
 
         element_t<TKey, TValue> *operate(TKey key, Operation op, TValue value)
         {
-            auto idx = hash(key);
-            element_t<TKey, TValue> *temp = &(elements[idx]);
+            auto idx0 = hash(key, hash0);
+            auto idx1 = hash(key, hash1);
+            element_t<TKey, TValue> *temp0 = &(elements_0[idx0]);
+            element_t<TKey, TValue> *temp1 = &(elements_1[idx1]);
+            element_t<TKey, TValue> *element = nullptr;
 
+            if (temp0->key == key && temp0->status == ES_POPULATED) element = temp0;
+            if (temp1->key == key && temp1->status == ES_POPULATED) element = temp1;
 
-            while (temp->key != key && temp->status != ES_EMPTY) {
-                idx = (idx + stride) % size;
-                temp = &(elements[idx]);
-            }
-
-            if (op == O_INSERT) {
-                if (temp->status == ES_EMPTY) {
-                    temp->key = key;
-                    temp->value = value;
-                    temp->status = ES_POPULATED;
+            if (!element && op == O_INSERT) {
+                // if either of the possible spots are free, then all is good!
+                // just stick the new value into there. "Free" means either
+                // empty, or deleted
+                if (temp0->status != ES_POPULATED) {
+                    temp0->key = key;
+                    temp0->value = value;
+                    temp0->status = ES_POPULATED;
                     count++;
-                } 
+                    return temp0;
+                }
+                else if (temp1->status != ES_POPULATED) {
+                    temp1->key = key;
+                    temp1->value = value;
+                    temp1->status = ES_POPULATED;
+                    count++;
+                    return temp1;
+                }
 
-                return temp;
+                // if not, evict temp1 back to the first array, evicting elements
+                // to their alternate array as needed until the insert succeeds,
+                // or the iteration limit is reached.
+
             }
-
-            return nullptr;
         }
 
 
-        u_int32_t hash(TKey key)
+        u_int32_t hash(TKey key, hashmode_t mode)
         {
-            u_int64_t hashed;
-
-            if (h0) {
-                hashed = h0(key);
-            } else {
-                hashed = (u_int64_t) key;
-            }
+            u_int64_t hashed = mode ? h1(key) : h0(key);
 
             return (hashed * size) >> 32;
         }
 
 
-        void initialize(int init_size, u_int32_t (*hfunc)(TKey), double max_loadfactor, int strd=1)
+        void initialize(size_t init_size, u_int32_t (*hfunc0)(TKey),
+                u_int32_t (*hfunc1)(TKey), size_t maxitr)
         {
             size = init_size;
-            h0 = hfunc;
-            max_lf = max_loadfactor;
-            stride = strd;
+            partial_sz = init_size / 2;
+            h0 = hfunc0;
+            h1 = hfunc1;
+            max_itr = maxitr;
 
-            elements = new element_t<TKey, TValue>[size];
-            for (int i=0; i<size; i++) {
-                elements[i] = element_t<TKey, TValue> {0, 0, ES_EMPTY};
+            elements_0 = new element_t<TKey, TValue>[partial_sz];
+            elements_1 = new element_t<TKey, TValue>[partial_sz];
+            for (size_t i=0; i<partial_sz; i++) {
+                elements_0[i] = element_t<TKey, TValue> {0, 0, ES_EMPTY};
+                elements_1[i] = element_t<TKey, TValue> {0, 0, ES_EMPTY};
             }
 
             count = 0;
@@ -123,24 +132,39 @@ class HashTable {
 
         void resize()
         {
-            int old_size = size;
-            size = size * 2;
-
             count = 0;
 
-            auto old_array = elements;
-            elements = new element_t<TKey, TValue>[size];
-            for (int i=0; i<size; i++) {
-                elements[i] = element_t<TKey, TValue> {0, 0, ES_EMPTY};
+            auto old_array0 = elements_0;
+            auto old_array1 = elements_1;
+
+            elements_0 = new element_t<TKey, TValue>[size];
+            elements_1 = new element_t<TKey, TValue>[size];
+
+
+            for (size_t i=0; i<size; i++) {
+                elements_0[i] = element_t<TKey, TValue> {0, 0, ES_EMPTY};
+                elements_1[i] = element_t<TKey, TValue> {0, 0, ES_EMPTY};
             }
 
-            for (int i=0; i<old_size; i++) {
-                if (old_array[i].status == ES_POPULATED) {
-                    operate(old_array[i].key, O_INSERT, old_array[i].value);
-                }
+            // yes, it's silly. I need to put a little more thought
+            // into this. The compiler will do strength reduction automatically
+            // on these anyway, right? Is it even worth it to store the half
+            // size somewhere, compared to just doing size / 2?
+            size_t old_sz = size;
+            size = size * 2;
+            partial_sz = size / 2;
+
+            for (size_t i=0; i<old_sz; i++) {
+                if (old_array0[i].status == ES_POPULATED)
+                    operate(old_array0[i].key, O_INSERT, old_array0[i].value);
+
+                if (old_array1[i].status == ES_POPULATED)
+                        operate(old_array1[i].key, O_INSERT, old_array1[i].value);
             }
 
-            delete old_array;
+
+            delete old_array0;
+            delete old_array1;
         }
 
 
@@ -151,25 +175,20 @@ class HashTable {
         }
 
 
-        HashTable<TKey, TValue>(int init_size, u_int32_t (*hfunc)(TKey))
+        HashTable<TKey, TValue>(size_t init_size, u_int32_t (*hfunc)(TKey))
         {
             initialize(init_size, hfunc, .75);
         }
 
 
-        HashTable<TKey, TValue>(int init_size, u_int32_t (*hfunc)(TKey), int strd)
+
+        HashTable<TKey, TValue>(size_t init_size, u_int32_t (*hfunc)(TKey), size_t maxitr)
         {
-            initialize(init_size, hfunc, .75, strd);
+            initialize(init_size, hfunc, maxitr);
         }
 
 
-        HashTable<TKey, TValue>(int init_size, u_int32_t (*hfunc)(TKey), int strd, double maxlod)
-        {
-            initialize(init_size, hfunc, maxlod, strd);
-        }
-
-
-        HashTable<TKey, TValue>(int init_size)
+        HashTable<TKey, TValue>(size_t init_size)
         {
             initialize(init_size, nullptr, .75);
         }
@@ -183,7 +202,7 @@ class HashTable {
 
         element_t<TKey, TValue> *insert(TKey key, TValue value)
         {
-            if (load_factor() > max_lf) resize();
+            if (load_factor() > max_itr) resize();
 
             return operate(key, O_INSERT, value);
         }
@@ -201,25 +220,15 @@ class HashTable {
         }
 
 
-        int length() { return count; }
+        size_t length() { return count; }
 
 
         double load_factor() {return (double) count / (double) size; }
 
 
-        double average_chain()
-        {
-            double total = 0;
-            for (int i=0; i<size; i++) {
-                total += elements[i].count;
-            }
-
-            return total / (double) size;
-        }
-
-
         ~HashTable()
         {
-            delete elements;
+            delete elements_0;
+            delete elements_1;
         }
 };
