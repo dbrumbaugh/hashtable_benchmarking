cf3d015e8cc69d06fb8de5e0f4088dfdb152d693
Douglas Rumbaugh <doug@douglasrumbaugh.com>
2021-07-14 12:20:07 -0400

    Cuckoo Benchmarks

--- benchmarks/bench_run.sh
+++ benchmarks/bench_run.sh
@@ -1,28 +1,16 @@
 #!/bin/ksh
 echo "Running benchmarks"
 
-
 echo -e "\tRunning baseline benchmark"
 echo "n,throughput,latency" > ./benchmarks/baseline.log
 for (( i = 1; i<1000000; i = $i + 10000 )); do
-    ./benchmarks/baseline_bench $i >> ./benchmarks/baseline.log
+#    ./benchmarks/baseline_bench $i >> ./benchmarks/baseline.log
 done
 echo -e "\tFinished running baseline benchmark"
 
-echo -e "\tRunning stride benchmark"
-echo "stride,throughput,latency" > ./benchmarks/stride.log
-for (( i = 1; i<100; i = $i + 1 )); do
-    ./benchmarks/stride_bench $i 100000 >> ./benchmarks/stride.log
+echo -e "\tRunning mixedops benchmark"
+echo "maxitr,write_mix,latency" > ./benchmarks/mixops.log
+for (( i = 1; i<101; i = $i + 1 )); do
+    ./benchmarks/mixedops_bench 10 $(( $i / 100.0  ))  >> ./benchmarks/mixops.log
 done
-echo -e "\tFinished running stride benchmark"
-
-
-echo -e "\tRunning lf benchmark"
-echo "lf,throughput,latency" > ./benchmarks/load.log
-for (( i = 50; i<91; i = $i + 1 )); do
-    lf=$(( $i / 100.0 ))
-    ./benchmarks/load_bench $lf 10000 >> ./benchmarks/load.log
-done
-echo -e "\tFinished running lf benchmark"
-
-echo ""
+echo -e "\tFinished running mixedops benchmark"
---
+++ benchmarks/mixedops_bench.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <chrono>
+#include "hashtable.hpp"
+
+using namespace std;
+
+uint32_t testhash0(uint32_t hval)
+{
+    hval = ((hval >> 16) ^ hval) * 0x45d9f3b;
+    hval = ((hval >> 16) ^ hval) * 0x45d9f3b;
+    hval = (hval >> 16) ^ hval;
+
+    return hval;
+}
+
+
+uint32_t testhash1(uint32_t hval)
+{
+    hval = ((hval >> 14) ^ hval) * 0x29a9f2b;
+    hval = ((hval >> 14) ^ hval) * 0x29a9f2b;
+    hval = (hval >> 14) ^ hval;
+
+    return hval;
+}
+
+
+HashTable<uint32_t, int> *initialize_table(size_t n, size_t maxitr)
+{
+    auto table = new HashTable<uint32_t, int>(&testhash0, &testhash1, maxitr);
+    for (size_t i=0; i<n; i++) {
+        table->insert(i, rand());
+    }
+
+    return table;
+}
+
+
+int main(int argc, char **argv)
+{
+    if (argc < 3) {
+        cerr << "useage: mixedops <maxitr> <write-mix>";
+        exit(1);
+    }
+
+    size_t maxitr = stoi(argv[1]);
+    double write_mix = stod(argv[2]);
+
+    auto test = initialize_table(1000000, maxitr);
+
+    double experiment_total = 0;
+    for (size_t i=0; i<1000; i++) {
+        double write = (double) rand() / (double) RAND_MAX;
+        uint32_t key = rand();
+        int value = rand();
+
+
+        if (write < write_mix) {
+            auto start = std::chrono::high_resolution_clock::now();
+            auto val = test->insert(key, value);
+            auto stop = std::chrono::high_resolution_clock::now();
+
+            experiment_total += chrono::duration_cast<chrono::nanoseconds>(stop - start).count();
+        } else {
+            auto start = std::chrono::high_resolution_clock::now();
+            auto val = test->access(key);
+            auto stop = std::chrono::high_resolution_clock::now();
+
+            experiment_total += chrono::duration_cast<chrono::nanoseconds>(stop - start).count();
+        }
+    }
+
+    delete test;
+
+    //tp_total *= 1e-9;
+    experiment_total *= 1e-9;
+
+    cout << maxitr << ",";
+    cout << write_mix << ",";
+    cout << (experiment_total / 1000) << "\n";
+}
